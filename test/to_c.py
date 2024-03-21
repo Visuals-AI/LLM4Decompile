db8ec454c6bd120b86c4fe6aa0ac9b2475dac602
EXP <289065406@qq.com>
2024-03-21 09:12:57 +0800

    add test

---
+++ test/to_c.py
@@ -0,0 +1,13 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+model_path = 'O:\Downloads\llm4'
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForCausalLM.from_pretrained(model_path,torch_dtype=torch.bfloat16).cuda()
+
+with open('EnableSlashAtCommands.asm','r') as f:#original file
+    asm_func = f.read()
+inputs = tokenizer(asm_func, return_tensors="pt").to(model.device)
+with torch.no_grad():
+    outputs = model.generate(**inputs, max_new_tokens=500)
+c_func_decompile = tokenizer.decode(outputs[0][len(inputs[0]):-1])
